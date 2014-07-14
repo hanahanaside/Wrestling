ee7bacdf42d4067c3595393f970db4a5891abda8
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-07-14 10:46:12 +0900

    Revert 890127f..53a1f3e
    This rolls back to commit 890127fdd1765385fdb5ee5edb23d178a559ec76.

--- Assets/Bandman/Scripts/Common/ReleaseChecker.cs
+++ Assets/Bandman/Scripts/Common/ReleaseChecker.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class ReleaseChecker : MonoBehaviour {
 
 	public static bool CheckOnSale(){
-		DateTime releaseDateTime =  DateTime.Parse("2014/7/20");
+		DateTime releaseDateTime =  DateTime.Parse("2014/7/24");
 		TimeSpan timeSpan = DateTime.Now - releaseDateTime;
 		if(timeSpan.Seconds >0){
 			return true;
--- Assets/Bandman/Scripts/Test/Test.cs
+++ Assets/Bandman/Scripts/Test/Test.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 
 public class Test : MonoBehaviour {
+
+	public GameObject youTubeViewPrefab;
+	public GameObject uiRoot;
 	
-	public UISprite sprite;
-
-	void Start(){
-
+	public void OnButtonClick(){
+		Debug.Log("ass");
+		GameObject youTubeViewObject = Instantiate(youTubeViewPrefab) as GameObject;
+		youTubeViewObject.transform.parent = uiRoot.transform;
+		youTubeViewObject.transform.localPosition = new Vector3 (0, 0, 0);
+		youTubeViewObject.transform.localScale = new Vector3 (1, 1, 1);
+		youTubeViewObject.BroadcastMessage("Show",1);
 	}
 
 
