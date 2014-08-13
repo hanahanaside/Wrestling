1cff4ad3dfdb16bfe144e917eb857ac0493a649e
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-08-13 17:17:14 +0900

    Revert a533619..02fb1c0
    This rolls back to commit a5336198a52f3491c7b9000dc08809ff96dae5ae.

--- Assets/Bandman/Scripts/Test/Test.cs
+++ Assets/Bandman/Scripts/Test/Test.cs
@@ -1,22 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
-using AppDriver.Android;
 
 public class Test : MonoBehaviour {
 
-	private int siteId = 15483;
-	private string siteKey = "c0aa7a91246177f405056e78a765a235";
-
-	void Start () {
-		Debug.Log ("Start");
-		AppDriverTracker.requestAppDriver (siteId, siteKey);
+	public GameObject youTubeViewPrefab;
+	public GameObject uiRoot;
+	
+	public void OnButtonClick(){
+		Debug.Log("ass");
+		GameObject youTubeViewObject = Instantiate(youTubeViewPrefab) as GameObject;
+		youTubeViewObject.transform.parent = uiRoot.transform;
+		youTubeViewObject.transform.localPosition = new Vector3 (0, 0, 0);
+		youTubeViewObject.transform.localScale = new Vector3 (1, 1, 1);
+		youTubeViewObject.BroadcastMessage("Show",1);
 	}
 
-	public void OnButtonClick () {
-		Hashtable ht = new Hashtable ();
-		ht.Add (AppDriverTracker.MEDIA_ID, 3437);
-		ht.Add (AppDriverTracker.IDENTIFIER, "test");
-		AppDriverTracker.openOfferwall (ht);
-	}
 
 }
